9a33c59036e871e970036de92fec0d59575b3d77
MIKOLAJ63 <MIKOLAJ63>
2017-08-19 17:29:43 +0200

    git commit -m Mentors amendments" cd ..

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,18 @@
 // scripts.js
-$function(){
+$(function(){
 	console.log('DOM loaded - you can have fun');
 });
 
 var text = 'Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.';
 
 //coarse
-var partOftext = text.slice(0, 72.5);
-var partOftextCharsAfter = partOftext.replace('Velociraptor', 'Triceratops');
+var partOftext = text.slice(0, 73.5);
 var dinosaur = 'Triceratops';
-var dinosaurUpperCased = dinosaur.toUpperCase();
+var dinosaurUpperCased = dinosaur.toUpperCase(); 
+var partOftextCharsAfter = partOftext.replace('Velociraptor', dinosaurUpperCased );
 console.log(partOftextCharsAfter);
 
 // scripts.js
 //cut
-var effect = text.replace('Velociraptor', 'Triceratops'.toUpperCase()).slice(0, text.length/2);
+var effect = text.replace('Velociraptor', dinosaurUpperCased).slice(0, text.length/2);
 console.log(effect);
